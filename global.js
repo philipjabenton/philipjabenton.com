f32867598c5d8b88aa546d71dbce2261c336766b
Philip J.A Benton <philip@philipjabenton.com>
2026-03-10 16:00:49 +0000

    Update global.js

--- global.js
+++ global.js
@@ -90,7 +90,7 @@ addEventListener("DOMContentLoaded", () => {
     gsap.to(nav, {
       yPercent: -100,
       duration: 0.35,
-      delay: 0.5,
+      delay: 0.15,
       ease: "power1.inOut",
       onComplete: () => window.location.href = url
     });
